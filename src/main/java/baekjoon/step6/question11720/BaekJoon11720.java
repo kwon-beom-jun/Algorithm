f07b2b25ffa08cd321dfd29c604d42bdf717a87a
qjawn <qjawn@192.168.1.101>
2022-07-24 16:22:05 +0900

    22.07.24 BaekJoon Step6 Upload

---
+++ src/main/java/baekjoon/step6/question11720/BaekJoon11720.java
@@ -0,0 +1,26 @@
+package baekjoon.step6.question11720;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BaekJoon11720 {
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		StringBuffer result = new StringBuffer();
+		int num = 0;
+		
+        // (첫째 줄 숫자의 개수 == 둘째 줄에 숫자 개수) 동일하므로 첫번째는 필요없음.
+		reader.readLine();
+		result.append(reader.readLine());
+		reader.close();
+		
+		for (int i = 0; i < result.toString().length(); i++) {
+			num += Integer.parseInt(String.valueOf(result.toString().charAt(i)));
+		}
+		
+		System.out.println(num);
+		
+	}
+}
---
+++ src/main/java/baekjoon/step6/question1316/BaekJoon1316.java
@@ -0,0 +1,56 @@
+package baekjoon.step6.question1316;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BaekJoon1316 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		int forNum =  Integer.parseInt(reader.readLine());
+		String[] readarr = new String[forNum];
+		// next == -1 : start
+		// next == -2 : break
+		int next = -1;
+		int result = 0;
+		
+		for (int i = 0; i < forNum; i++) {
+			readarr[i] = reader.readLine();
+		}
+		
+		for (int i = 0; i < readarr.length; i++) {
+			
+			String readStr = readarr[i];
+			
+			for (int j = 0; j < readStr.length(); j++) {
+				
+				next = -1;
+				
+				if (!(readStr.indexOf(String.valueOf(readStr.charAt(j))) == readStr.lastIndexOf(String.valueOf(readStr.charAt(j))))) {
+					for (int z = 0; z < readStr.length(); z++) {
+						if (String.valueOf(readStr.charAt(j)).equals(String.valueOf(readStr.charAt(z)))) {
+							if (next == -1 || (next != z && next == z-1)) {
+								next = z;
+							}else {
+								next = -2;
+								break;
+							}
+						}
+					}	
+				}
+				if (next == -2) {
+					break;
+				}
+				if (j == readStr.length()-1 && next != -2) {
+					result++;
+				}
+				
+				
+			}
+		}
+
+		System.out.println(result);
+		
+	}
+}
