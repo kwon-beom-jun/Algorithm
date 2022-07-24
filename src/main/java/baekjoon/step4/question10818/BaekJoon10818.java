cd00cc29029bf106b28b896f48372cfe4b40bdbe
qjawn <qjawn@192.168.1.101>
2022-07-24 16:04:13 +0900

    22.07.24 BaekJoon Step4 Upload

---
+++ src/main/java/baekjoon/step4/question10818/BaekJoon10818.java
@@ -0,0 +1,36 @@
+package baekjoon.step4.question10818;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BaekJoon10818 {
+	public static void main(String[] args) {
+		
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		
+		try {
+			
+			int num = Integer.parseInt(bf.readLine());
+			String str[] = bf.readLine().split(" ");
+			
+			int max = -1000000;
+			int min = 1000000;
+			
+			for (int i = 0; i < num; i++) {
+                if (max < Integer.parseInt(str[i])) {
+                    max = Integer.parseInt(str[i]);
+                }
+                if (min > Integer.parseInt(str[i])) {
+                    min = Integer.parseInt(str[i]);
+                }
+            }
+			System.out.println(min + " " + max);
+		} catch (NumberFormatException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+}
