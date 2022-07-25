3c03ba37f88f92058d48d93ec77e1dedbc65beba
qjawn <qjawn@DESKTOP-CKN3KU7>
2022-07-25 21:03:19 +0900

    22.07.25 BaekJoon Step7 Upload

---
+++ src/main/java/baekjoon/step7/question2869/BaekJoon2869.java
@@ -0,0 +1,16 @@
+package baekjoon.step7.question2869;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BaekJoon2869 {
+	public static void main(String[] args) throws IOException {
+		
+		String[] question = new BufferedReader(new InputStreamReader(System.in)).readLine().split(" ");
+		int A = Integer.parseInt(question[0]), B = Integer.parseInt(question[1]), C = Integer.parseInt(question[2]);
+		
+		System.out.println((C - B)%(A - B) == 0 ? (C - B)/(A - B) : (C - B)/(A - B) + 1);
+		
+	}
+}
